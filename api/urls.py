f791104d84a59f24c00b5dfd695218baf80a64d5
srinivas0101 <srinivasguptha0101@gmail.com>
2024-08-02 20:27:13 +0530

    Prepare for deployment

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 # api/urls.py
 from django.urls import path
+from . import views
 from .views import bfhl_post, bfhl_get
 
 urlpatterns = [
-    path('bfhl', bfhl_post),
-    path('bfhl', bfhl_get),
+    path('api/bfhl/', bfhl_post, name='bfhl_post'),  # POST request
+    path('api/bfhl/get/', bfhl_get, name='bfhl_get')
 ]
